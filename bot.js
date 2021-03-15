0547b4545144318bd5a413fd95532a97f7a9cf73
carrotishpickle <andymasox@gmail.com>
2021-03-15 20:51:44 +0100

    First build
    Adds very basic functionality, which is replying to =ping by Pong!

--- bot.js
+++ bot.js
@@ -1,12 +1,15 @@
 const Discord = require('discord.js');
+const config = require('./config.json');
 const client = new Discord.Client();
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-if (message.content === '=ping') {
-	// send back "Pong." to the channel the message was sent in
-	message.channel.send('Pong!');
-}
-// i know there is no token here but i think you understand why
+client.on('message', message => {
+	if (message.content === config.prefix + 'ping') {
+		message.channel.send('Pong!');
+	}
+});
+
+client.login(config.token);
